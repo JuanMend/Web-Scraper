32e27f65e909cf7ac598c682a58a4d6ea9716588
JuanMend <juanmend690@gmail.com>
2020-03-20 18:38:55 -0500

    parse

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-
+import json
 url = 'http://ethans_fake_twitter_site.surge.sh/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
+tweetArr = []
 for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
     tweetObj = {
         "author": tweet.find('h2', attrs={"class": "author"}).text.encode('utf-8'),
@@ -12,5 +13,6 @@ for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
         "likes": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
         "shares": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
     }
-
-print(tweetObj)
+    tweetArr.append(tweet)
+    with open('twitterData.json', 'w') as outfile:
+        json.dump(tweetArr, outfile)
