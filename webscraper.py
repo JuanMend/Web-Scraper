c12d9a30273df2f0b48922c2bf334196b3f3d093
JuanMend <juanmend690@gmail.com>
2020-03-20 17:45:34 -0500

    webscrape

--- webscraper.py
+++ webscraper.py
@@ -4,6 +4,13 @@ import requests
 url = 'http://ethans_fake_twitter_site.surge.sh/'
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
+for tweet in content.findAll('div', attrs={"class": "tweetcontainer"}):
+    tweetObj = {
+        "author": tweet.find('h2', attrs={"class": "author"}).text.encode('utf-8'),
+        "date": tweet.find('h5', attrs={"class": "dateTime"}).text.encode('utf-8'),
+        "tweet": tweet.find('p', attrs={"class": "content"}).text.encode('utf-8'),
+        "likes": tweet.find('p', attrs={"class": "likes"}).text.encode('utf-8'),
+        "shares": tweet.find('p', attrs={"class": "shares"}).text.encode('utf-8')
+    }
 
-
-print(content)
+print(tweetObj)
